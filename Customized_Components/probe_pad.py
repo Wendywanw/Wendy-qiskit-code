b44bdfcd8604952ed87a6446a321bf0b9e9b2f08
Wendy <50036145+Wendywanw@users.noreply.github.com>
2024-10-15 10:23:10 -0700

    made probe pad chips and big dolan junctions

--- Customized_Components/probe_pad.py
+++ Customized_Components/probe_pad.py
@@ -1,4 +1,5 @@
 
+
 # -*- coding: utf-8 -*-
 
 # This code is part of Qiskit.
@@ -27,8 +28,8 @@ import Transmon_specifications as jj
 
 
 
-class DolanJunctionBandage(QComponent):
-    """A dolan junction that complies with the Lincoln lab design rules with bandage.
+class ProbePad(QComponent):
+    """A dose array coponent 
 
     Inherits QComponent class.
 
@@ -55,29 +56,14 @@ class DolanJunctionBandage(QComponent):
     default_options = Dict(
         pos_x = '0um', 
         pos_y = '0um',
-        w_pad_pin = '2 um',
-        w_pad_u = '2.8 um',
-        d_pin = '250 nm',
-        d_u = '600 nm',
-        total_length = '10 um',
-        w_top_pin = '250 nm',
-        w_top_u = '300 nm',
-        w_bot_pin = '200 nm',
-        w_bot_u = '300 nm',
-        top_bot_offset = '0 nm' ,
-        jj_extra = '400nm',
-        jj_gap = '0.2um',
-        fillet = '30 nm',
-        uc_override_pad = '300nm',        # jj_gap_actual = '0.2um',
-        # Lj = '10',
-        resolution = '5',
-        # Jc = '0.1',
+        w_pocket = '350 um',
+        h_pocket = '200 um',
+        w_pad = '150 um',
+        h_pad = '100 um',
+        pad_gap = '7 um',
         orientation = '0',
-        pin_layer = '0',
-        gap_area_layer = '1',
-        dimension_text = 'False',
-        jj_orientation = '180',
-        uc_override = '30nm',)
+        layer = '0',
+        )
     """Default drawing options"""
 
     component_metadata = Dict(short_name='Pocket',
@@ -102,63 +88,33 @@ class DolanJunctionBandage(QComponent):
 
         # extract chip name
         chip = p.chip
-        special_jj = (np.absolute((p.orientation - p.jj_orientation)%180) == 90)
 
         # since we will reuse these options, parse them once and define them as varaibles
-        w_pad_pin = p.w_pad_pin
-        w_pad_u = p.w_pad_u
-        d_pin = p.d_pin
-        d_u = p.d_u
+        w_pocket = p.w_pocket
+        h_pocket = p.h_pocket
+        w_pad = p.w_pad
+        h_pad = p.h_pad
+        pad_gap = p.pad_gap
 
-        w_top_pin = p.w_top_pin
-        w_top_u = max(p.w_top_u,w_top_pin+2*p.uc_override)
-        w_bot_pin = p.w_bot_pin
-        w_bot_u = max(p.w_bot_u,w_bot_pin+2*p.uc_override)
+        pocket = rec2(w_pocket, h_pocket)
+        pad = rec2(w_pad, h_pad)
 
-        jj_extra = p.jj_extra
+        pad_left = draw.translate(pad, -w_pad/2-pad_gap/2, 0)
+        pad_right = draw.translate(pad, w_pad/2 + pad_gap/2, 0)
 
-        top_bot_offset = p.top_bot_offset
-        total_height = p.total_length
-        jj_gap = p.jj_gap
-        finger_length = total_height - jj_gap
-        top_finger_len = finger_length/2-top_bot_offset + jj_extra
-        bot_finger_len = finger_length/2+top_bot_offset + jj_extra
+        all = draw.union(pad_left, pad_right)
+        cut = pocket
 
-        resolution = p.resolution
+        components = draw.rotate([all,cut], p.orientation, origin=(0,0))
+        all, cut = draw.translate(components, p.pos_x, p.pos_y)
 
-        pad_pin = rec2(w_pad_pin, d_pin,)
-        pad_u = rec2(w_pad_u, d_u)
-
-        top_pin = rec2(w_top_pin, top_finger_len)
-        bot_pin = rec2(w_bot_pin, bot_finger_len)
-
-        finger_u = rec2(max(w_top_u,w_bot_u), total_height+jj_extra*2+p.uc_override_pad*2)
-
-        top_pad_pin = draw.translate(pad_pin, 0, total_height/2-d_pin/2)
-        bot_pad_pin = draw.translate(pad_pin, 0, -total_height/2+d_pin/2)
         
-        top_pad_u = draw.translate(pad_u, 0, total_height/2-d_pin/2)
-        bot_pad_u = draw.translate(pad_u, 0, -total_height/2+d_pin/2)
-
-        top_finger_pin = draw.translate(top_pin, 0, top_finger_len/2+jj_gap/2 )
-        bot_finger_pin = draw.translate(bot_pin, 0, -bot_finger_len/2-jj_gap/2)
-
-        all = draw.shapely.ops.unary_union([top_pad_pin, bot_pad_pin, top_finger_pin, bot_finger_pin])
-
-        under_cut  = draw.shapely.ops.unary_union([top_pad_u, bot_pad_u,finger_u])
-
-        cut = under_cut.difference(all)
         
-        components = draw.rotate([all, cut], p.orientation, origin = (0,0))
-        all,cut = draw.translate(components, p.pos_x, p.pos_y)
-        
-        if p.dimension_text:
-            pass
         # Use the geometry to create Metal qgeometry
         self.add_qgeometry('poly',
                            dict(all=all),
-                           chip=chip, layer = p.pin_layer)
+                           chip=chip, layer = p.layer)
 
         self.add_qgeometry('poly', dict(cut = cut), 
-                           subtract=False, layer=p.gap_layer,
+                           subtract=True, layer=p.layer,
                            chip=chip)
