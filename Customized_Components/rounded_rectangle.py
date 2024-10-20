96a1a0f38b4a865e50a7244bb28b59e297ee0b80
Wendy <50036145+Wendywanw@users.noreply.github.com>
2024-10-20 00:21:14 -0700

    added comment for rounded rectangle function

--- Customized_Components/rounded_rectangle.py
+++ Customized_Components/rounded_rectangle.py
@@ -21,7 +21,7 @@ import numpy as np
 
 
 class RoundedRectangle(QComponent):
-    """A rrectangle with rounded corners.
+    """A rectangle with rounded corners.
 
     Inherits QComponent class.
 
@@ -51,8 +51,8 @@ class RoundedRectangle(QComponent):
         pos_y = '0um',
         width = '200um',
         height = '200um',
-        radius='50um',
-        resolution='5',
+        radius ='50um',
+        resolution ='5',
         cap_style='round',
         connection = False,
         connection_radius = '10um',
@@ -89,10 +89,8 @@ class RoundedRectangle(QComponent):
                 y_corner = p.height/2*yi-radius*yi
                 pts = np.array(pts)
                 coords = pts +np.array([x_corner,y_corner])
-                if i == 0:
-                    index = coords
-                else:
-                    index = np.concatenate((index,coords))
+                index = None
+                index = coords if i == 0 else np.concatenate((index,coords))
             index = np.concatenate((index,np.array([[p.width/2,-p.height/2],[-p.width/2,-p.height/2]])))
 
         else:
@@ -107,14 +105,11 @@ class RoundedRectangle(QComponent):
                 pts = np.array(pts)
                 coords = pts +np.array([x_corner,y_corner])
                 # coords = list(coords)
-                if i == 0:
-                    index = coords
-                else:
-                    index = np.concatenate((index,coords))
-                # print(index)
+                index = coords if i == 0 else np.concatenate((index,coords))
+                        # print(index)
 
         rounded_rec = draw.Polygon(index)
-        
+
         if p.connection:
             rounded_rec = draw.rotate(rounded_rec,p.connection_direction,overwrite= True, origin=(0,0))
         rounded_rec = draw.translate(rounded_rec,p.pos_x,p.pos_y,overwrite= True)
@@ -126,6 +121,20 @@ class RoundedRectangle(QComponent):
                            chip=p.chip)
         
 def rounded_rec_only(width, height, radius, resolution = 5, connection = False,connection_direction = 180):
+    """_summary_
+
+    Args:
+        width (_type_): the size of the rectangle in x direction
+        height (_type_): the size of the rectangle in y direction
+        radius (_type_): the radius of the rounded corner
+        resolution (int, optional): number of points making up the rounded corner. Defaults to 5.
+        connection (bool, optional): if the rectangle connects to a different rectangle, check this. Defaults to False.
+        connection_direction (int, optional): which direction the connection is to. Rotation counter clockwise starting from pos x axis. Defaults to 180.
+
+    Returns:
+        _type_: Polygon object
+    """
+    
     radius = min(radius,height/2,width/2)
     if radius>max(height/2,width/2):
         print('Error: radius is too big. Will show up as circle')
@@ -141,10 +150,8 @@ def rounded_rec_only(width, height, radius, resolution = 5, connection = False,c
             y_corner = height/2*yi-radius*yi
             pts = np.array(pts)
             coords = pts +np.array([x_corner,y_corner])
-            if i == 0:
-                index = coords
-            else:
-                index = np.concatenate((index,coords))
+            index = None
+            index = coords if i == 0 else np.concatenate((index,coords))
         index = np.concatenate((index,np.array([[width/2,-height/2],[-width/2,-height/2]])))
 
     else:
@@ -159,24 +166,40 @@ def rounded_rec_only(width, height, radius, resolution = 5, connection = False,c
             pts = np.array(pts)
             coords = pts +np.array([x_corner,y_corner])
             # coords = list(coords)
-            if i == 0:
-                index = coords
-            else:
-                index = np.concatenate((index,coords))
-            # print(index)
+            index = coords if i == 0 else np.concatenate((index,coords))
+                    # print(index)
 
     rounded_rec = draw.Polygon(index)
-    
+
     if connection:
         rounded_rec = draw.rotate(rounded_rec,connection_direction,overwrite= True, origin=(0,0))
     return rounded_rec
     # rounded_rec = draw.translate(rounded_rec,p.pos_x,p.pos_y,overwrite= True)
 def rounded_rec(width, height, r1=0,r2 = 0,r3 = 0,r4 = 0, resolution = 5,d1 = [-1,-1],d2 = [1,-1],d3 = [1,1],d4 = [-1,1], same_radius = False, r = 5):
+    """Generate customized rounded rectangle with different radius and rounded corners to face different directions. 
+    the order of corners is from the same naming as quadrants in a cartesian plane.
+    Args:
+        width (_type_): the size of the rectangle in y direction
+        height (_type_): the size of the rectangle in x direction
+        r1 (int, optional): radius of the first rounded corner. Defaults to 0.
+        r2 (int, optional): radius of the second rounded corner. Defaults to 0.
+        r3 (int, optional): radius of the third rounded corner.. Defaults to 0.
+        r4 (int, optional): radius of the fourth rounded corner.. Defaults to 0.
+        resolution (int, optional): the number of points that makes up the rounded corner. Defaults to 5.
+        d1,d2,d3,d4: the direction of the corner, the direction is where the center of circle is facing w.r.t. the line. [x direction, y direction] +1 means positive axis direction, -1 means negative axis direction.
+        d1 (list, optional): direction of corner 1. Defaults to [-1,-1].
+        d2 (list, optional): direction of corner 2. Defaults to [1,-1].
+        d3 (list, optional): direction of corner 3. Defaults to [1,1].
+        d4 (list, optional): direction of corner 4. Defaults to [-1,1].
+        same_radius (bool, optional): True of the corners have the same radius. Defaults to False.
+        r (int, optional): if the corners have same radius, then this sets the radius. Defaults to 5.
+
+    Returns:
+        _type_: Polygon object
+    """
+    
     directions = np.array([[-1,-1],[1,-1],[1,1],[-1,1]])
-    if same_radius:
-        radius = np.array([r,r,r,r])
-    else:
-        radius = np.array([r1,r2,r3,r4])
+    radius = np.array([r,r,r,r]) if same_radius else np.array([r1,r2,r3,r4])
     d = np.array([d1,d2,d3,d4])
     x = np.array([-width/2, width/2, width/2, -width/2])
     y = np.array([-height/2, -height/2, height/2, height/2])
@@ -191,17 +214,12 @@ def rounded_rec(width, height, r1=0,r2 = 0,r3 = 0,r4 = 0, resolution = 5,d1 = [-
             diff = directions[i]-d[i]
             xx = xx + diff[0]*radius[i]
             yy = yy + diff[1]*radius[i]
-            
+
             coord = ((round_corners(xx,yy,direction = d[i], radius = radius[i], n_points = resolution)))
             inds = np.argsort(-coord[:,0]*directions[i][1]*directions[i][0]*d[i][0])
             coord = coord[inds]
-        if i == 0:
-            coords = coord
-        else:
-            coords = np.concatenate((coords, coord))
-
-    rounded_rec = draw.Polygon(coords)
-    return rounded_rec
+        coords = coord if i == 0 else np.concatenate((coords, coord))
+    return draw.Polygon(coords)
 
 def round_corners(x,y,direction, radius, n_points = 5):
     xi, yi = direction
@@ -212,6 +230,4 @@ def round_corners(x,y,direction, radius, n_points = 5):
     x_corner = x#-radius*xi
     y_corner = y#-radius*yi
     pts = np.array(pts)
-    coords = pts +np.array([x_corner,y_corner])
-    
-    return coords
+    return pts +np.array([x_corner,y_corner])
